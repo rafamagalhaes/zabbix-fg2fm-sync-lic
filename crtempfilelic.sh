b88349ad5d3e1ea7c5102e7960f228c94c599647
rafamagalhaes <41116777+rafamagalhaes@users.noreply.github.com>
2020-07-31 13:45:57 -0300

    Update crtempfilelic.sh

--- crtempfilelic.sh
+++ crtempfilelic.sh
@@ -9,4 +9,4 @@
 ## Created: May, 31 2020   Rafael Magalhães      Unknown changes              ##
 ##----------------------------------------------------------------------------##
 
-sshpass -p $2 ssh -q -o StrictHostKeyChecking=no $1@177.154.136.140 diagnose fmupdate dbcontract fgd > /var/tmp/zabbix/fortimanagerlic.tmp
+sshpass -p $2 ssh -q -o StrictHostKeyChecking=no $1@$3 diagnose fmupdate dbcontract fgd > /var/tmp/zabbix/fortimanagerlic.tmp
